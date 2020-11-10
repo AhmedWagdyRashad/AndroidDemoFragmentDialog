3f69a20802e4cda7e3a588b867bbc85d30260895
Ahmed Wagdy Rashad <a7med.wagdy96@gmail.com>
2020-11-10 09:21:14 +0200

    adding custom action bar

--- app/src/main/java/com/ahmedwagdy/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/ahmedwagdy/dialogfragment/MainActivity.kt
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.tool_bar)
+        setSupportActionBar(toolbar)
         fab.setOnClickListener{
 
         }
