432f410df75c8e1d676bc1ecda61669f82ba29a0
Ahmed Wagdy Rashad <a7med.wagdy96@gmail.com>
2020-11-10 02:03:19 +0200

    creating a simple dialog

--- app/src/main/java/com/ahmedwagdy/dialogfragment/AlertDialogFragment.kt
+++ app/src/main/java/com/ahmedwagdy/dialogfragment/AlertDialogFragment.kt
@@ -1,4 +1,21 @@
 package com.ahmedwagdy.dialogfragment
 
-class AlertDialogFragment {
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+
+class AlertDialogFragment() : DialogFragment() {
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val alertDialog = AlertDialog.Builder(activity!!).create()
+        with(alertDialog){
+            setTitle("Alert")
+            setMessage("Here's some important information!")
+            setButton(AlertDialog.BUTTON_NEUTRAL,"OK") { dialog, which ->
+                dialog.dismiss()
+            }
+        }
+        return alertDialog
+    }
 }
--- app/src/main/java/com/ahmedwagdy/dialogfragment/MainActivity.kt
+++ app/src/main/java/com/ahmedwagdy/dialogfragment/MainActivity.kt
@@ -2,10 +2,22 @@ package com.ahmedwagdy.dialogfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab.setOnClickListener{
+            showDialog()
+        }
+    }
+
+    private fun showDialog() {
+        val dialogFragment = AlertDialogFragment()
+        with(dialogFragment){
+            isCancelable = false
+            show(supportFragmentManager,"DIALOG_FRAGMENT")
+        }
     }
 }
